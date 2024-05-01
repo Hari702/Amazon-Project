db12726b0362e09cd3de7eda7ba2bc2133a9a583
Hari702 <hariips70707@gmail.com>
2024-05-01 09:30:05 +0530

    Modified cartTest.js and orderSummaryTest.js using cart-class.js

--- tests/checkout/orderSummaryTests.js
+++ tests/checkout/orderSummaryTests.js
@@ -1,6 +1,5 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
-
-import { cart, loadFromStorage } from "../../data/cart.js";
+import { cart } from "../../data/cart-class.js";
 import { renderPaymentSummary } from "../../scripts/checkout/paymentSummary.js";
 
 
@@ -12,8 +11,8 @@ describe("test suite: display Order summary", () => {
 
         spyOn(localStorage,"setItem")
 
-        spyOn(localStorage, "getItem").and.callFake(() => {
-            return JSON.stringify([{
+        cart.cartItems=[
+            {
                 productid: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
                 quantity: 1,
                 deliveryOptionId: "1"
@@ -25,10 +24,9 @@ describe("test suite: display Order summary", () => {
                 deliveryOptionId: "1"
 
 
-            }])
-        })
-
-        loadFromStorage()
+            }
+        ]
+        
 
         renderOrderSummary()
     })
@@ -45,52 +43,52 @@ describe("test suite: display Order summary", () => {
 
         expect(document.querySelectorAll(".ordered-product-container").length).toEqual(2)
 
-        console.log(cart[0].quantity)
+        console.log(cart.cartItems[0].quantity)
 
-        expect(document.querySelector(`.quantity-${cart[0].productid}`).innerHTML).toContain("Quantity:1")
+        expect(document.querySelector(`.quantity-${cart.cartItems[0].productid}`).innerHTML).toContain("Quantity:1")
 
-        expect(document.querySelector(`.quantity-${cart[1].productid}`).innerHTML).toContain("Quantity:2")
+        expect(document.querySelector(`.quantity-${cart.cartItems[1].productid}`).innerHTML).toContain("Quantity:2")
 
        
 
-        expect(document.querySelector(`.js-cart-item-name-${cart[0].productid}`).innerHTML).toEqual("Black and Gray Athletic Cotton Socks - 6 Pairs")
+        expect(document.querySelector(`.js-cart-item-name-${cart.cartItems[0].productid}`).innerHTML).toEqual("Black and Gray Athletic Cotton Socks - 6 Pairs")
 
-        expect(document.querySelector(`.js-cart-item-name-${cart[1].productid}`).innerHTML).toEqual("Intermediate Size Basketball")
+        expect(document.querySelector(`.js-cart-item-name-${cart.cartItems[1].productid}`).innerHTML).toEqual("Intermediate Size Basketball")
 
 
-        expect(document.querySelector(`.js-price-${cart[0].productid}`).innerHTML).toEqual("100")
+        expect(document.querySelector(`.js-price-${cart.cartItems[0].productid}`).innerHTML).toEqual("100")
 
-        expect(document.querySelector(`.js-price-${cart[1].productid}`).innerHTML).toEqual("500")
+        expect(document.querySelector(`.js-price-${cart.cartItems[1].productid}`).innerHTML).toEqual("500")
         
     })
 
     it("remove a product",()=>{
 
-        document.querySelector(`.js-delete-btn-${cart[0].productid}`).click()
+        document.querySelector(`.js-delete-btn-${cart.cartItems[0].productid}`).click()
 
         expect(document.querySelectorAll(".ordered-product-container").length).toEqual(1)
 
         expect(document.querySelector(".ordered-product-container-e43638ce-6aa0-4b85-b27f-e1d07eb678c6")).toEqual(null)
 
-        expect(document.querySelector(`.ordered-product-container-${cart[0].productid}`)).not.toEqual(null)
+        expect(document.querySelector(`.ordered-product-container-${cart.cartItems[0].productid}`)).not.toEqual(null)
 
-        expect(cart.length).toEqual(1)
-        expect(cart[0].productid).toEqual("15b6fc6f-327a-4ec4-896f-486349e85a3d")
+        expect(cart.cartItems.length).toEqual(1)
+        expect(cart.cartItems[0].productid).toEqual("15b6fc6f-327a-4ec4-896f-486349e85a3d")
         
-        expect(document.querySelector(`.js-cart-item-name-${cart[0].productid}`).innerHTML).toEqual("Intermediate Size Basketball")
-        expect(document.querySelector(`.js-price-${cart[0].productid}`).innerHTML).toEqual("500")
+        expect(document.querySelector(`.js-cart-item-name-${cart.cartItems[0].productid}`).innerHTML).toEqual("Intermediate Size Basketball")
+        expect(document.querySelector(`.js-price-${cart.cartItems[0].productid}`).innerHTML).toEqual("500")
 
     })
 
     it("updating delivery option",()=>{
         console.log(document.querySelector(".order-summary-container"))
 
-        document.querySelector(`.js-delivery-option-${cart[0].productid}-3`).click()
+        document.querySelector(`.js-delivery-option-${cart.cartItems[0].productid}-3`).click()
 
-        expect(document.querySelector(`.js-delivery-option-${cart[0].productid}-3`).checked).toEqual(true)
+        expect(document.querySelector(`.js-delivery-option-${cart.cartItems[0].productid}-3`).checked).toEqual(true)
 
         renderPaymentSummary()
-        expect(cart[0].deliveryOptionId).toEqual("3")
+        expect(cart.cartItems[0].deliveryOptionId).toEqual("3")
 
         expect(document.querySelector(".js-payment-summary-delivery-price").innerHTML).toEqual("90")
         expect(document.querySelector(".js-total-price").innerHTML).toEqual("1,309")
--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -1,4 +1,4 @@
-import { cart, addToCart, updateDeliveryOption } from "../../data/cart.js";
+import { cart } from "../../data/cart-class.js";
 import { loadFromStorage } from "../../data/cart.js";
 import { removeProductFromCart } from "../../data/cart.js"
 
@@ -9,7 +9,7 @@ describe("test suite: add to cart", () => {
 
     beforeEach(() => {
         spyOn(localStorage, "setItem")
-        document.querySelector(".select-class").innerHTML=`<select class="js-quantity-selector-e43638ce-6aa0-4b85-b27f-e1d07eb678c6">
+        document.querySelector(".select-class").innerHTML = `<select class="js-quantity-selector-e43638ce-6aa0-4b85-b27f-e1d07eb678c6">
         <option selected value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -35,7 +35,7 @@ describe("test suite: add to cart", () => {
       </select>`
     })
 
-    afterEach(()=>{
+    afterEach(() => {
         document.querySelector(".js-quantity-selector-e43638ce-6aa0-4b85-b27f-e1d07eb678c6").remove()
         document.querySelector(".js-quantity-selector-15b6fc6f-327a-4ec4-896f-486349e85a3d").remove()
     })
@@ -43,50 +43,49 @@ describe("test suite: add to cart", () => {
     it("add a existing product to cart", () => {
 
 
-        spyOn(localStorage, "getItem").and.callFake(() => {
-            return JSON.stringify([{
-                productid: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-                quantity: 1,
-                deliveryOptionId: "1"
+        cart.cartItems = [{
+            productid: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            quantity: 1,
+            deliveryOptionId: "1"
 
-            }])
-        })
+        }]
 
         loadFromStorage()
 
-        console.log(cart[0])
+    console.log(cart[0])
 
-        addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6")
-        expect(cart[0].quantity).toEqual(2)
+    cart.addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6")
+    expect(cart.cartItems[0].quantity).toEqual(2)
 
-        expect(localStorage.setItem).toHaveBeenCalledWith("cartproduct", JSON.stringify([{
-            productid: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            quantity: 2,
-            deliveryOptionId: "1"
+    expect(localStorage.setItem).toHaveBeenCalledWith("cartproduct-oop", JSON.stringify([{
+        productid: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2,
+        deliveryOptionId: "1"
 
-        }]))
-
-
+    }]))
     })
+
+    
+
 
     it("add a new product to cart", () => {
 
-        spyOn(localStorage, "getItem").and.callFake(() => {
-            return JSON.stringify([])
-        })
 
-        loadFromStorage()
+        cart.cartItems = []
+
+
+
         console.log(localStorage.getItem("cart"))
 
-        addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6")
-        addToCart("15b6fc6f-327a-4ec4-896f-486349e85a3d")
+        cart.addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6")
+        cart.addToCart("15b6fc6f-327a-4ec4-896f-486349e85a3d")
 
-        expect(cart[0].productid).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6")
-        expect(cart[1].productid).toEqual("15b6fc6f-327a-4ec4-896f-486349e85a3d")
+        expect(cart.cartItems[0].productid).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6")
+        expect(cart.cartItems[1].productid).toEqual("15b6fc6f-327a-4ec4-896f-486349e85a3d")
         expect(localStorage.setItem).toHaveBeenCalledTimes(2)
-        expect(cart.length).toEqual(2)
+        expect(cart.cartItems.length).toEqual(2)
 
-        expect(localStorage.setItem).toHaveBeenCalledWith("cartproduct", JSON.stringify([{
+        expect(localStorage.setItem).toHaveBeenCalledWith("cartproduct-oop", JSON.stringify([{
             productid: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
             quantity: 1,
             deliveryOptionId: "1"
@@ -99,8 +98,9 @@ describe("test suite: add to cart", () => {
 
         }]))
     })
-})
 
+
+})
 
 
 describe("test suite: remove from the cart", () => {
@@ -108,36 +108,32 @@ describe("test suite: remove from the cart", () => {
     beforeEach(() => {
 
         spyOn(localStorage, "setItem")
-        spyOn(localStorage, "getItem").and.callFake(() => {
-            return JSON.stringify([
-                {
-                    productid: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-                    quantity: 1,
-                    deliveryOptionId: "1"
-                },
-                {
-                    productid: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-                    quantity: 1,
-                    deliveryOptionId: "1"
+        cart.cartItems = [
+            {
+                productid: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                quantity: 1,
+                deliveryOptionId: "1"
+            },
+            {
+                productid: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+                quantity: 1,
+                deliveryOptionId: "1"
 
 
-                }
-            ])
-        })
-        loadFromStorage()
-
+            }
+        ]
     })
     it("remove the product in the cart", () => {
 
-        removeProductFromCart(cart[0].productid)
+        cart.removeProductFromCart(cart.cartItems[0].productid)
 
         console.log(cart)
 
-        expect(cart.length).toEqual(1)
-        expect(cart[0].productid).toEqual("15b6fc6f-327a-4ec4-896f-486349e85a3d")
+        expect(cart.cartItems.length).toEqual(1)
+        expect(cart.cartItems[0].productid).toEqual("15b6fc6f-327a-4ec4-896f-486349e85a3d")
 
         expect(localStorage.setItem).toHaveBeenCalledTimes(1)
-        expect(localStorage.setItem).toHaveBeenCalledWith("cartproduct", JSON.stringify([{
+        expect(localStorage.setItem).toHaveBeenCalledWith("cartproduct-oop", JSON.stringify([{
             productid: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
             quantity: 1,
             deliveryOptionId: "1"
@@ -149,11 +145,11 @@ describe("test suite: remove from the cart", () => {
 
     it("does nothing if product not in cart", () => {
 
-        removeProductFromCart("83d4ca15-0f35-48f5-b7a3-1ea210004f2e")
+        cart.removeProductFromCart("83d4ca15-0f35-48f5-b7a3-1ea210004f2e")
 
-        expect(cart.length).toEqual(2)
+        expect(cart.cartItems.length).toEqual(2)
         expect(localStorage.setItem).toHaveBeenCalledTimes(1)
-        expect(localStorage.setItem).toHaveBeenCalledWith("cartproduct", JSON.stringify([
+        expect(localStorage.setItem).toHaveBeenCalledWith("cartproduct-oop", JSON.stringify([
             {
                 productid: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
                 quantity: 1,
@@ -177,31 +173,28 @@ describe("test suite: update delivery option", () => {
     beforeEach(() => {
 
         spyOn(localStorage, "setItem")
-        spyOn(localStorage, "getItem").and.callFake(() => {
-            return JSON.stringify([
-                {
-                    productid: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-                    quantity: 1,
-                    deliveryOptionId: "1"
-                },
-                {
-                    productid: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-                    quantity: 1,
-                    deliveryOptionId: "1"
+        cart.cartItems = [
+            {
+                productid: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                quantity: 1,
+                deliveryOptionId: "1"
+            },
+            {
+                productid: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+                quantity: 1,
+                deliveryOptionId: "1"
 
-                }
-            ])
-        })
-        loadFromStorage()
+            }
+        ]
+
     })
-
 
     it("update delivery option for product in the cart", () => {
 
-        updateDeliveryOption(cart[0].productid, "3")
-        expect(cart[0].deliveryOptionId).toEqual("3")
+        cart.updateDeliveryOption(cart.cartItems[0].productid, "3")
+        expect(cart.cartItems[0].deliveryOptionId).toEqual("3")
         expect(localStorage.setItem).toHaveBeenCalledTimes(1)
-        expect(localStorage.setItem).toHaveBeenCalledWith("cartproduct", JSON.stringify([{
+        expect(localStorage.setItem).toHaveBeenCalledWith("cartproduct-oop", JSON.stringify([{
             productid: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
             quantity: 1,
             deliveryOptionId: "3"
@@ -219,24 +212,28 @@ describe("test suite: update delivery option", () => {
     })
 
 
-    it("does nothing if the product is not in the cart",()=>{
-        updateDeliveryOption("83d4ca15-0f35-48f5-b7a3-1ea210004f2e", "3")
+    it("does nothing if the product is not in the cart", () => {
+        cart.updateDeliveryOption("83d4ca15-0f35-48f5-b7a3-1ea210004f2e", "3")
 
-        expect(cart[0].deliveryOptionId).toEqual("1")
-        expect(cart[1].deliveryOptionId).toEqual("1")
-        
+        expect(cart.cartItems[0].deliveryOptionId).toEqual("1")
+        expect(cart.cartItems[1].deliveryOptionId).toEqual("1")
+
 
         expect(localStorage.setItem).toHaveBeenCalledTimes(0)
     })
 
 
-    it("does nothing if the delivery option does not exist",()=>{
-        updateDeliveryOption("e43638ce-6aa0-4b85-b27f-e1d07eb678c6", "4")
+    it("does nothing if the delivery option does not exist", () => {
+        cart.updateDeliveryOption("e43638ce-6aa0-4b85-b27f-e1d07eb678c6", "4")
 
-        expect(cart[0].deliveryOptionId).toEqual("1")
-        expect(cart[1].deliveryOptionId).toEqual("1")
-        
+        expect(cart.cartItems[0].deliveryOptionId).toEqual("1")
+        expect(cart.cartItems[1].deliveryOptionId).toEqual("1")
+
 
         expect(localStorage.setItem).toHaveBeenCalledTimes(0)
     })
+
+
 })
+
+
